06d3c1b2955dc4f195ba15a26b02e176829352f8
anshi10 <anshita.bhasin@dnata.com>
2020-07-12 14:11:21 +0400

    Missing Numbers in Array and Swapping Of String.

---
+++ src/test/java/Java_Questions/DuplicateElementsInArray.java
@@ -0,0 +1,34 @@
+/*
+package Java_Questions;
+
+public class DuplicateElementsInArray {
+    public static void main(String[] args) {
+
+        String a[] = { "Anshita", "Ankita", "Aba", "Mumma", "Nani", "Papa","Mumma" };
+        String x[] = null ;
+        String y[] = null;
+        for (int i = 0; i < a.length; i++) {
+            x = x.add(a[i]);
+
+            System.out.println(
+                y == x ? a[i] + " is duplicate element in array list " : "");
+            y = x;
+        }
+
+        */
+/*int a[] = { 1, 2, 4, 4, 8, 9 };
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < a.length; i++) {
+            x = a[i];
+
+            System.out.println(
+                y == x ? a[i] + " is duplicate element in array list " : "");
+            y = x;
+        }*//*
+
+
+    }
+
+}
+*/
